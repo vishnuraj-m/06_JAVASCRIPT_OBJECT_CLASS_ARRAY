1bad63bfa83d6ad9c63205e471efb5efd5d4505c
vishnuacappella <acappella.studiov@gmail.com>
2022-07-11 12:07:19 +0530

    change

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
-class Car {
+// class Car {
+//     constructor(model, make) {
+//         this.make = make;
+//         this.model = model;
+//     }
+// }
 
+
+// !
+// console.log(window.innerWidth);
+// console.log(window.innerHeight);
+// window.alert('this is an alert!')
+// const name = window.prompt('What is your name?');
+// const name = prompt('What is your name?');
+// console.log(name);
+
+// !/*********/
+
+const car = {
+    make: "VolksWagen",
+    model: "Polo",
+    move: function () {
+        console.log('moving')
+    }
 }
+console.log(car);
+console.log(car.make);
+console.log(car.model);
+car.move()
